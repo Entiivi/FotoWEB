870a9db8562698be1d2d77085b75df827dbb9154
Entiivi <ignas.sakalauskasss@gmail.com>
2025-02-12 22:41:22 +0200

    3
    3

--- FotoKlubasSvetaine.Server/obj/Debug/net8.0/FotoKlubasSvetaine.Server.AssemblyInfo.cs
+++ FotoKlubasSvetaine.Server/obj/Debug/net8.0/FotoKlubasSvetaine.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FotoKlubasSvetaine.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87a2dc2f1e7762a7146373dac343d9c87fa8ede")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b970bdf885e691713b17aa17a2b5ca9b249205")]
 [assembly: System.Reflection.AssemblyProductAttribute("FotoKlubasSvetaine.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FotoKlubasSvetaine.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
